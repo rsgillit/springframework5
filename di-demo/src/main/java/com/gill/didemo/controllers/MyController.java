79eeb97b01d0ceee2d690624bd442ab000772a74
rsgillit <rsgillit@gmail.com>
2019-12-21 13:22:34 -0600

    @Qualifier, @Primary and @Profile

--- di-demo/src/main/java/com/gill/didemo/controllers/MyController.java
+++ di-demo/src/main/java/com/gill/didemo/controllers/MyController.java
@@ -13,7 +13,7 @@ public class MyController {
     }
 
     public String hello() {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
         return greetingService.sayGreeting();
     }
 
