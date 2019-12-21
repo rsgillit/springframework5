e29e365230bfc02d35546cde15e52cf96e4dca93
rsgillit <rsgillit@gmail.com>
2019-12-21 15:57:40 -0600

    @Qualifier, @Primary and @Profile

--- di-demo/src/main/java/com/gill/didemo/services/PrimaryGermanGreetingService.java
+++ di-demo/src/main/java/com/gill/didemo/services/PrimaryGermanGreetingService.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+
+
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Greeting in German";
     }
 }
