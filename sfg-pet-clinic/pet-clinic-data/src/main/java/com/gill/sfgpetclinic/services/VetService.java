d455f7d235c3fa72e38299f8f814a4bc75c2faf0
rsgillit <rsgillit@gmail.com>
2019-12-23 09:52:07 -0600

    Refactor Service Interface to Common Base Interface. closes #11.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/VetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/VetService.java
@@ -2,13 +2,7 @@ package com.gill.sfgpetclinic.services;
 
 import com.gill.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
 
-    Vet findById(Long id) ;
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
 }
