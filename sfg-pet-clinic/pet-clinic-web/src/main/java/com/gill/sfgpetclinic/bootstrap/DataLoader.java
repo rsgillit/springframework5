c6513635b79d5e68aaad3292f06159377e4674be
rsgillit <rsgillit@gmail.com>
2019-12-24 12:39:28 -0600

    Add PetType in with Bootstrap (startup). closes #32.

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/gill/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/gill/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package com.gill.sfgpetclinic.bootstrap;
 
 import com.gill.sfgpetclinic.model.Owner;
+import com.gill.sfgpetclinic.model.PetType;
 import com.gill.sfgpetclinic.model.Vet;
 import com.gill.sfgpetclinic.services.OwnerService;
+import com.gill.sfgpetclinic.services.PetService;
+import com.gill.sfgpetclinic.services.PetTypeService;
 import com.gill.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,11 +15,15 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
-        System.out.println("I am intialized");
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+        System.out.println("I am initialized");
     }
 
     @Override
@@ -25,9 +32,19 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void initOnStartup() {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+
 
         ownerService.save(owner1);
 
